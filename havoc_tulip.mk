b9b93fe64f14a80c31176d90dc60622f855ba9a0
Akhil MS <akhilsree001@gmail.com>
2021-02-21 12:18:39 +0530

    Update build fingerprint to coral Feb

--- havoc_tulip.mk
+++ havoc_tulip.mk
@@ -38,4 +38,4 @@ TARGET_VENDOR_PRODUCT_NAME := tulip
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V10.3.2.0.PEKMIXM release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys"
